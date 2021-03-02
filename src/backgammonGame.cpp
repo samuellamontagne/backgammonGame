27fe7ac6bf88682d00e68ca1f2787293e89e0bc6
Qin <qin.shao.ca@gmail.com>
2021-03-03 03:58:47 +0800

    update Player player could roll dice get 2 integer

--- src/backgammonGame.cpp
+++ src/backgammonGame.cpp
@@ -7,11 +7,15 @@
 //============================================================================
 
 #include <iostream>
+#include "Players.h"
+
 using namespace std;
 
 int main() {
 
-
-
-	return 0;
+ Players play1("jim");
+ play1.rollDice();
+ cout <<play1.getDice1()<<endl;
+ cout <<play1.getDice2()<<endl;
+ return 0;
 }
