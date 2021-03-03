71b28ffe80b4fc68505e0b5feefa188639a8b985
Qin <qin.shao.ca@gmail.com>
2021-03-04 03:30:23 +0800

    modify print()

--- src/backgammonGame.cpp
+++ src/backgammonGame.cpp
@@ -238,8 +238,10 @@ int main() {
 
 			//Roll the dices
 
-			dice1 = rollDice();
-			dice2 = rollDice();
+//			dice1 = rollDice();
+//			dice2 = rollDice();
+			dice1 = 5;
+			dice2 = 4;
 			captured = gameBoard.getCaptured2();
 
 			//Ask which dice to play
